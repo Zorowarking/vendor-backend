238fa51b1153db77cf885c8de957f715bf97ddfd
Zorowarking <zaidun.khn4444@gmail.com>
2026-05-10 03:50:45 +0530

    fix: add mock customer token to auth_optional to allow guest cart merging

--- server/middleware/auth_optional.js
+++ server/middleware/auth_optional.js
@@ -14,11 +14,22 @@ const firebaseAuthOptional = async (req, res, next) => {
   const idToken = authHeader.split('Bearer ')[1];
 
   try {
+    // DEV MOCK: Vendor Token
     if (idToken === 'mock-session-token-123') {
       req.user = {
         uid: 'mock-uid-123',
         phoneNumber: '+919999999999',
         email: 'dev@test.com'
+      };
+      return next();
+    }
+
+    // DEV MOCK: Customer Token
+    if (idToken === 'mock-customer-token-123') {
+      req.user = {
+        uid: 'mock-uid-customer-123',
+        phoneNumber: '+917777777777',
+        email: 'customer@test.com'
       };
       return next();
     }
